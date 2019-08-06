c1174c47977ba1eee1b8d1fcd6034a78ff3e925b
braywolf11 <brayfer1893@gmail.com>
2019-08-05 21:51:01 -0500

    basic calc

---
+++ hello.py
@@ -0,0 +1,9 @@
+#this is my fisrt python program
+#Developer: Brayan F. Rivera
+a = 5
+b = 10
+print (":: BASIC CALC ::")
+print (" the add is: ", a + b )
+print (" the subs is: ", a - b )
+print (" the div is: ", a / b )
+print (" the mult is: ", a * b )
