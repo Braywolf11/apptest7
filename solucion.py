7ea1cf75956ff5efdb050c3b01107fd5cff07815
braywolf11 <brayfer1893@gmail.com>
2019-08-12 21:59:52 -0500

    tarea2

--- solucion.py
+++ solucion.py
@@ -1,9 +1,12 @@
 import os
 from random import randint, uniform, random
 def AL(x,y,z):
+    i = 0
+
     while i <= x:
         ansZ = randint(y , z)
-        return ansZ
+        i += 1
+        print ("este es ", ansZ)
 
 #main###################################···· 
 os.system("clear")
@@ -13,4 +16,4 @@ l2 = int(input("upper limit: "))
 print ("::: MENU :::")
 print ("[1]. imprimir")
 opt = int(input("Press an option: "))
-print("The answer is: ", AL())
+print("The answer is: ", AL(n,l1,l2))
