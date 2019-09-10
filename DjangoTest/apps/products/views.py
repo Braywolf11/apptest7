6eacbc8eb12253337b7b92637ecc5505284b065e
braywolf11 <brayfer1893@gmail.com>
2019-09-09 21:54:07 -0500

    actualizacion

--- DjangoTest/apps/products/views.py
+++ DjangoTest/apps/products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("::: Welcome to my market :::")
+
+def hello(request):
+    return HttpResponse("::: Hello everyone:::")
